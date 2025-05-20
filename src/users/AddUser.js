ec2382c186dddff4601a0d29b010de3484b1134d
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-21 11:24:47 +1200

    add add user form inputs

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,4 +1,32 @@
 
 export default function AddUser() {
-  return <div>Adduser form</div>
+  return <div className="container">
+    <div className="row">
+      <div className="col-md-6 offset-md-3 border rounded p-4 mt-2">
+        <h2 className="text-center m-4">
+          Register user
+        </h2>
+        <div className="mb-3">
+          <label htmlFor="Name" className="form-label">
+            Name
+          </label>
+          <input type={"text"} className="form-control" placeholder="Enter your name" name="name" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Username" className="form-label">
+            Username
+          </label>
+          <input type={"text"} className="form-control" placeholder="Enter your username" name="username" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Email" className="form-label">
+            Email
+          </label>
+          <input type={"text"} className="form-control" placeholder="Enter your email" name="email" />
+        </div>
+        <button type="submit" className="btn btn-outline-primary">Submit</button>
+        <button type="reset" className="btn btn-outline-danger mx-2">Cancel</button>
+      </div>
+    </div >
+  </div >
 }
