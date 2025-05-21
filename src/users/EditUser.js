5be509b1f8c61dc3391291dd6e15edbf54290625
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-21 14:03:42 +1200

    feat: add front-end edit functionality

---
+++ src/users/EditUser.js
@@ -0,0 +1,89 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+
+export default function EditUser() {
+
+  let navigate = useNavigate()
+  const { id } = useParams()
+  const [user, setUser] = useState(
+    {
+      name: "",
+      username: "",
+      email: ""
+    }
+  )
+
+  const handleClear = (e) => {
+    navigate("/")
+    // setUser(
+    //   {
+    //     name: "",
+    //     username: "",
+    //     email: ""
+    //   }
+    // )
+  }
+
+  const handleChange = (e) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  useEffect(() => {
+    loadUser()
+  }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await axios.put(`http://localhost:8080/user/${id}`, user)
+    navigate("/")
+
+    setUser(
+      {
+        name: "",
+        username: "",
+        email: ""
+      }
+    )
+  }
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:8080/user/${id}`, user)
+    setUser(result.data)
+  }
+
+  return <div className="container">
+    <div className="row">
+      <div className="col-md-6 offset-md-3 border rounded p-4 mt-2">
+        <h2 className="text-center m-4">
+          Edit user
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="Name" className="form-label">
+              Name
+            </label>
+            <input type={"text"} className="form-control" placeholder="Enter your name" name="name" value={user.name} onChange={handleChange} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Username" className="form-label">
+              Username
+            </label>
+            <input type={"text"} className="form-control" placeholder="Enter your username" name="username" value={user.username} onChange={handleChange} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Email" className="form-label">
+              Email
+            </label>
+            <input type={"text"} className="form-control" placeholder="Enter your email" name="email" value={user.email} onChange={handleChange} />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">Submit</button>
+          <button type="reset" className="btn btn-outline-danger mx-2" onClick={handleClear}>Cancel</button>
+        </form>
+      </div>
+    </div >
+  </div >
+}
